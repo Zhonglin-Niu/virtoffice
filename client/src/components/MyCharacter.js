e35cf66a2ffc31149bc778f375a18d29abfd376e
Zhonglin-Niu <zn23@calvin.edu>
2024-03-08 18:02:31 -0500

    character movement

--- client/src/components/MyCharacter.js
+++ client/src/components/MyCharacter.js
@@ -1,12 +1,12 @@
-import React, {useEffect, useContext} from 'react';
-import {connect} from 'react-redux';
+import React, { useEffect, useContext } from 'react';
+import { connect } from 'react-redux';
 
 import CanvasConext from './CanvasContext';
-import {CHARACTER_IMAGE_SIZE, CHARACTER_CLASSES_MAP} from './characterConstants';
-import {TILE_SIZE} from './mapConstants';
-import {loadCharacter} from './slices/statusSlice';
+import { CHARACTER_IMAGE_SIZE, CHARACTER_CLASSES_MAP } from './characterConstants';
+import { TILE_SIZE } from './mapConstants';
+import { loadCharacter } from './slices/statusSlice';
 import { MY_CHARACTER_INIT_CONFIG } from './characterConstants';
-import {update as updateAllCharactersData} from './slices/allCharactersSlice'
+import { update as updateAllCharactersData } from './slices/allCharactersSlice'
 
 
 function MyCharacter({ myCharactersData, loadCharacter, updateAllCharactersData, webrtcSocket }) {
@@ -27,6 +27,59 @@ function MyCharacter({ myCharactersData, loadCharacter, updateAllCharactersData,
         if (context == null || myCharactersData == null) {
             return;
         }
+
+        const handleKeyPress = (event) => {
+            const { keyCode } = event;
+            const { position } = myCharactersData;
+
+            let newX = position.x;
+            let newY = position.y;
+
+            // Update character's position based on key pressed
+            switch (keyCode) {
+                case 65: // A key
+                case 37: // Left arrow key
+                    newX -= 1;
+                    break;
+                case 87: // W key
+                case 38: // Up arrow key
+                    newY -= 1;
+                    break;
+                case 68: // D key
+                case 39: // Right arrow key
+                    newX += 1;
+                    break;
+                case 83: // S key
+                case 40: // Down arrow key
+                    newY += 1;
+                    break;
+                default:
+                    break;
+            }
+
+            event.preventDefault();
+
+            updateAllCharactersData({
+                [MY_CHARACTER_INIT_CONFIG.id]: {
+                    ...myCharactersData,
+                    position: { x: newX, y: newY },
+                },
+            });
+        };
+
+        document.addEventListener('keydown', handleKeyPress);
+
+        // Clean up event listener on component unmount
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [context, myCharactersData, updateAllCharactersData]);
+
+    useEffect(() => {
+        if (context == null || myCharactersData == null) {
+            return;
+        }
+
         const characterImg = document.querySelector(`#character-sprite-img-${MY_CHARACTER_INIT_CONFIG.characterClass}`);
         const { sx, sy } = CHARACTER_CLASSES_MAP[MY_CHARACTER_INIT_CONFIG.characterClass].icon;
         context.canvas.drawImage(
@@ -47,9 +100,9 @@ function MyCharacter({ myCharactersData, loadCharacter, updateAllCharactersData,
 }
 
 const mapStateToProps = (state) => {
-    return {myCharactersData: state.allCharacters.users[MY_CHARACTER_INIT_CONFIG.id]};
+    return { myCharactersData: state.allCharacters.users[MY_CHARACTER_INIT_CONFIG.id] };
 };
 
-const mapDispatch = {loadCharacter, updateAllCharactersData};
+const mapDispatch = { loadCharacter, updateAllCharactersData };
 
 export default connect(mapStateToProps, mapDispatch)(MyCharacter);
